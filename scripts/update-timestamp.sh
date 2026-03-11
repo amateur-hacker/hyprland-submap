6f55f129db028b6876cec19ca9229f3d210d9137
amateur-hacker <amateurhacker2004@gmail.com>
2026-03-11 05:57:46 +0530

    fix: use Z suffix for UTC timestamp

--- scripts/update-timestamp.sh
+++ scripts/update-timestamp.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-TIMESTAMP=$(date -u +"%Y-%m-%dT%H:%M:%S%z")
+TIMESTAMP=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 sed -i "s/\"lastUpdated\": \"[^\"]*\"/\"lastUpdated\": \"$TIMESTAMP\"/" registry.json
