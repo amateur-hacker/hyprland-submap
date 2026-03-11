fab1c7a8d6f153ccc99452725d9852341ed3c77c
amateur-hacker <amateurhacker2004@gmail.com>
2026-03-11 06:03:03 +0530

    fix: use GitHub timezone offset format (+00:00)

--- scripts/update-timestamp.sh
+++ scripts/update-timestamp.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-TIMESTAMP=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+TIMESTAMP=$(date -u +"%Y-%m-%dT%H:%M:%S%z")
 sed -i "s/\"lastUpdated\": \"[^\"]*\"/\"lastUpdated\": \"$TIMESTAMP\"/" registry.json
